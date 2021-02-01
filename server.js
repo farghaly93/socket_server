f6341ffe83a8faba3d696cb79be09f5b35b6fcaa
farghaly <miserable.farghaly93@gmail.com>
2021-02-01 02:43:09 +0200

    first

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);;
+const io = require('socket.io')(server);
+const port = 6969;
+
+
+
+io.sockets.on('connection', (socket) => {
+  console.log('Connection', socket.id);
+  socket.emit('socketId', socket.id);
+
+  socket.on('send', async(messageData) => {
+    const rooms = io.sockets.adapter.rooms[messageData.room];
+    io.sockets.in(messageData.room).emit('message', newMessage);
+  });
+
+
+
+  socket.on('join', data => {
+    const room = data.room;
+    socket.join(room);
+    const clientsLength = io.nsps['/'].adapter.rooms[room].length;
+    if(clientsLength > 0) {
+      io.sockets.to(room).emit('newUser', {message: `${data.username} has joined the chat..`, joined: true, joiners: clientsLength});
+      socket.emit('newUser', {message: `Welcome to this chat room.. ${data.username}`, joined: true, joiners: clientsLength});
+    } else {
+      socket.emit('newUser', {joined: false});
+    }
+  }); 
+
+
+});
+
+
+
+  server.listen(process.env.PORT || port, () => {
+    console.log('Server started and connected to port: '+port);
+  });
